2d58fe5855b91cfaf54ad5df3a43786abc5e7555
AbdallaM2 <119325558+AbdallaM2@users.noreply.github.com>
2023-06-21 23:41:18 +0300

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ PICS = (environ.get('PICS' ,'https://telegra.ph/file/42467dcbba60b89bdfcc3.jpg h
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '324012925').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
